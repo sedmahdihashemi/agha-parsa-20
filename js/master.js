fadcc4002c2ed3c59f83e12962fe9a921b333486
sedmahdihashemi <155801810+sedmahdihashemi@users.noreply.github.com>
2024-03-02 00:44:07 +0330

    syntax 20

---
+++ js/master.js
@@ -0,0 +1,23 @@
+document.write('halo world')
+console.log('salam donya');
+alert('salam be to')
+console.log(window);
+
+
+// const x =[1, 2, 3]
+// x[0]
+
+
+
+
+// const y ={
+//     name: 'mahdi' ,
+//     lname: 'hashemi' ,
+//     age: 22 ,
+//     eyecolor: 'black'
+
+
+
+// }
+// y.name
+// y.lname
